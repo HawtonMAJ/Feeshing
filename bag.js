bcd3c81612f1bfcd38f46bf418be9ca8bce9c15a
HawtonMAJ <hawtonmaj81421@gmail.com>
2026-06-11 13:35:39 -0600

    kil fish added

--- bag.js
+++ bag.js
@@ -49,19 +49,19 @@ pondBtn.addEventListener("click", () => {
 });
 
 // Add fish to inventory when pond is clicked
-pond.addEventListener("click", addItem)
-    
+pond.addEventListener("click", addItem);
+
 function addItem() {
     const item = inventoryItems[Math.floor(Math.random() * inventoryItems.length)];
     const slot = document.createElement("div");
     slot.classList.add("item-slot");
     slot.innerHTML = `
-        <div class="item-icon"><img src="${item.icon}"></div>
-        <div class="item-name">${item.name}</div>
-        <div class="item-rarity">${item.rarity}</div>
+    <div class="item-icon"><img src="${item.icon}"></div>
+    <div class="item-name">${item.name}</div>
+    <div class="item-rarity">${item.rarity}</div>
     `;
     inventoryGrid.appendChild(slot);
-
+    
     const toast = document.getElementById("toast");
     toast.innerHTML = `
     <div class="item-icon"><img src="${item.icon}"></div>
@@ -77,10 +77,22 @@ function resetHideTimer(){
     clearTimeout(hideTimer);
     toast.classList.remove("fade-out");
     const currentContent = toast.innerHTML;
-
+    
     hidetimer = setTimeout(() => {
         if (toast.innerHTML === currentContent) {
             toast.classList.add("fade-out");
         }
-    }, 2000);
+    }, 1000);
 }
+document.addEventListener("DOMContentLoaded", toast.classList.add("fade-out"));
+// Remove Fish from inventory and give money equal to rarity 5 for white 20 for green
+
+
+
+inventoryGrid.addEventListener("click", (event) => {
+    const itemSlot = event.target.closest(".item-slot")
+
+    if (itemSlot) {
+        itemSlot.remove();
+    }
+});
